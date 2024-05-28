54aa0b3e280a06d3ed51894178fd10a8dce53c23
shihab4513 <shihab4513@gmail.com>
2024-05-28 10:04:36 +0600

    PP1.js added

---
+++ PP1.js
@@ -0,0 +1,18 @@
+// Ans:No:01
+const multiply3 = (a, b, c) => a * b * c;
+const result = multiply3(5, 5, 5);
+console.log(result);
+
+
+// Ans:No:02
+
+const message = `I am a web developer.
+I love to code.
+i love to eat biryani`;
+console.log(message);
+
+// Ans:No:03
+
+const add2 = (a, b = 5) => a + b;
+const sum = add2(10);
+console.log(sum);
